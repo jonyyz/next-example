531d276c67cec48fe6bca66b8cca9c0720e1c2c8
Jon Cameron <jon.cameron.yyz@gmail.com>
2022-05-27 15:20:42 -0400

    Have to ignore tsx imports to use tsc.  Raging debate on github...

--- tests/components/message.test.tsx
+++ tests/components/message.test.tsx
@@ -1,5 +1,6 @@
 import { render } from "@testing-library/react";
 import StoreProvider from "@/store/index.js";
+// @ts-ignore
 import Message from "@/components/message.tsx";
 
 describe("Message Component", () => {
