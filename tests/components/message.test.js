1bd6864c40a8c49892bf91f239d07660676c1460
Jon Cameron <jon.cameron.yyz@gmail.com>
2022-05-27 14:56:26 -0400

    Wrap component test in StoreProvider

--- tests/components/message.test.js
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react";
-import Message from "@components/message.tsx";
-
-describe("Message Component", () => {
-  it("should render", () => {
-    const { container } = render(<Message message="test" />);
-    expect(container).toMatchSnapshot();
-  });
-});
---
+++ tests/components/message.test.tsx
@@ -0,0 +1,15 @@
+import { render } from "@testing-library/react";
+import StoreProvider from "@/store/index.js";
+import Message from "@/components/message.tsx";
+
+describe("Message Component", () => {
+  it("should render", () => {
+    // TODO: MockStoreProvider
+    const { container } = render(
+      <StoreProvider>
+        <Message message="test" />
+      </StoreProvider>
+    );
+    expect(container).toMatchSnapshot();
+  });
+});
